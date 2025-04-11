219dd651082344fd32b3917ebc5d5078c274606a
BrockBadeaux14 <bbadea9@lsu.edu>
2025-04-10 23:35:38 -0500

    Fix to correctly toggle line rendered OnDestroy of lure.

--- Assets/Scripts/Lure.cs
+++ Assets/Scripts/Lure.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 public class Lure : MonoBehaviour
 {
     public static event Action<Transform> OnLureCreated;
+    public static event Action OnLureDestroyed;
 
     private void Awake()
     {
         OnLureCreated?.Invoke(transform);
     }
 
+    private void OnDestroy()
+    {
+        OnLureDestroyed?.Invoke();
+    }
 }
